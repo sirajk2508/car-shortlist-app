66f015929f6917ce4c687172b52f8806d3938cbf
Siraj Khan <sirajkhan3649@gmail.com>
2026-05-31 22:16:45 +0530

    Updated the title

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
@@ -5,6 +6,10 @@ import Questionnaire from './pages/Questionnaire';
 import Results from './pages/Results';
 
 function App() {
+  useEffect(() => {
+    document.title = "CarShortlist";
+  }, []);
+
   return (
     <Router>
       <div className="min-h-screen bg-gray-50 text-gray-900 font-sans">
